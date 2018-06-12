9a8c687f1d9a8549589314006f40e79a30fe71ab
Anderson <andersonrojo312@gmail.com>
2018-06-12 17:24:11 -0500

    Cardview_perros

--- app/src/main/java/com/example/worldskills/adoptadogws/InicioActivity.java
+++ app/src/main/java/com/example/worldskills/adoptadogws/InicioActivity.java
@@ -1,5 +1,6 @@
-package com.example.worldskills.adoptadogws;
+package com.example.worldskills.adoptadogws.Interfaces;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,8 +14,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.worldskills.adoptadogws.R;
+
 public class InicioActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,22 +83,24 @@ public class InicioActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.nav_gallery){
+            startActivity(new Intent(this, PerrosActivity.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.nav_camera:
+                startActivity(new Intent(this, InicioActivity.class));
+                break;
+            case R.id.nav_gallery:
+                startActivity(new Intent(this, PerrosActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/worldskills/adoptadogws/Interfaces/DetailActivity.java
@@ -0,0 +1,44 @@
+package com.example.worldskills.adoptadogws.Interfaces;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.example.worldskills.adoptadogws.R;
+
+
+public class DetailActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String id = getIntent().getStringExtra(PerrosActivity.EXTRA_ID);
+
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+}
