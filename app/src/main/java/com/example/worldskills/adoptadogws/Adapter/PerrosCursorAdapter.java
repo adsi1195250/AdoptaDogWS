8ca219a50748279c2965a6b9c2e6e4f56b0c8c92
Brayan Tabares <brayantabares10a2014@gmail.com>
2018-06-12 17:27:11 -0500

    Database, Adapter

--- app/src/main/java/com/example/worldskills/adoptadogws/Adapter/PerrosCursorAdapter.java
+++ app/src/main/java/com/example/worldskills/adoptadogws/Adapter/PerrosCursorAdapter.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.worldskills.adoptadogws.Data.Relacion;
 import com.example.worldskills.adoptadogws.R;
 
 public class PerrosCursorAdapter extends CursorAdapter {
@@ -23,7 +25,19 @@ public class PerrosCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, Context context, Cursor c) {
         //TextView nombreText = view.findViewById(R.id.)
+        //TextView edadText = view.findViewById(R.id.)
+        //TextView razaText = view.findViewById(R.id.)
+        //TextView tipoText = view.findViewById(R.id.)
+        //final ImageView imageView = view.findViewById(R.id.id_imagen)
+
+
+        String nombre = c.getString(c.getColumnIndex(Relacion.GeneralEntry.NOMBRE));
+        String edad = c.getString(c.getColumnIndex(Relacion.GeneralEntry.EDAD));
+        String raza = c.getString(c.getColumnIndex(Relacion.GeneralEntry.RAZA));
+        String tipo = c.getString(c.getColumnIndex(Relacion.GeneralEntry.TIPO));
+        String imagen = c.getString(c.getColumnIndex(Relacion.GeneralEntry.IMAGEN));
+
     }
 }
